f9b9a76806d4d58a75fa2f211ec4fe02f8f8401f
Kim-wonder <glow2myself@gmail.com>
2022-07-25 12:09:34 +0900

    React 학습 파일 업로드 (BuketList)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import BucketList from "./BuketList";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>내 버킷리스트 :)</h2>
+      <BucketList/>
     </div>
   );
 }
